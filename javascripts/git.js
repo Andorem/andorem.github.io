58221b6ccf63cf0aaeef82d3e78b56657fbfed20
Andorem <andoremmc2@gmail.com>
2015-11-12 19:39:48 -0800

    Update git.js

--- javascripts/git.js
+++ javascripts/git.js
@@ -23,7 +23,7 @@
 				if ((this.description).indexOf(keyword) >= 0) {
 				list.append('<dt><a href="'+ (this.homepage?this.homepage:this.html_url) +'">' + this.name + '</a> <em>'+(this.language?('('+this.language+')'):'')+'</em></dt>');
 				list.append('<dd>' + this.description +'</dd>');
-				list.append('<dd><em>Size: '+(this.size<1000?(this.size+' kB'):(Math.round((this.size/1000)*100)/100+' MB'))+' - Watchers: '+this.watchers+' - Forks: '+this.forks+' </em></dd>');
+				list.append('<dd><em>Size: '+(/*this.size<1000?(this.size+' kB'):(Math.round((this.size/1000)*100)/100+' MB'))+' -*/ +'Watchers: '+this.watchers+' - Forks: '+this.forks+' </em></dd>');
 				list.append('<dd><br/></dd>');
 		  }
 			}
