babc54b52b99f87d864fdd75c61c7e3390ae6a25
Andorem <andoremmc2@gmail.com>
2015-11-12 16:49:46 -0800

    Update git.js

--- javascripts/git.js
+++ javascripts/git.js
@@ -8,8 +8,8 @@
 	  jQuery.getJSON('https://api.github.com/users/'+username+'/repos?callback=?',callback)
     }
 
-    jQuery.fn.loadRepositories = function(username) {
-      this.html("<span>Querying GitHub for " + username +"'s repositories...</span>");
+    jQuery.fn.loadRepositories = function(username, keyword) {
+      //this.html("<span>Querying GitHub for " + username +"'s repositories...</span>");
      
       var target = this;
       $.githubUser(username, function(data) {
@@ -19,8 +19,8 @@
         var list = $('<dl/>');
         target.empty().append(list);
         $(repos).each(function() {
-			if (this.name != (username.toLowerCase()+'.github.io')) {
-				list.append('<dt><a href="'+ (this.homepage?this.homepage:this.html_url) +'">' + this.name + '</a> <em>'+(this.language?('('+this.language+')'):'')+'</em></dt>');
+			if ((this.name != (username.toLowerCase()+'.github.io')) && ((this.description.toLowerCase().indexOf(keyword) >= 0)) {
+				list.append('<dt><h3><a href="'+ (this.homepage?this.homepage:this.html_url) +'">' + this.name + '</a></h3> <em>'+(this.language?('('+this.language+')'):'')+'</em></dt>');
 				list.append('<dd>' + this.description +'</dd>');
 				list.append('<dd><em>Size: '+(this.size<1000?(this.size+' kB'):(Math.round((this.size/1000)*100)/100+' MB'))+' - Watchers: '+this.watchers+' - Forks: '+this.forks+' </em></dd>');
 				list.append('<dd><br/></dd>');
